c6e90e33b590413b907234bc69b3049cf3e165ed
Avijit Ghosh <gavijit622@gmail.com>
2023-06-10 15:43:03 +0530

    Minor refactoring in gaurd

--- src/app/shared/guards/checkout.guard.ts
+++ src/app/shared/guards/checkout.guard.ts
@@ -1,33 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { map, Observable } from 'rxjs';
 import { State } from 'src/app/store';
 import * as fromReducers from 'src/app/store/reducers';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CheckoutGuard  {
-  constructor(
-    private store: Store<State>,
-    private router: Router,
-  ) {}
+export class CheckoutGuard {
+  constructor(private store: Store<State>, private router: Router) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | boolean {
-    return this.store.select(fromReducers.selectCart)
-      .pipe(
-        map((cart) => {
-          if (cart.items && cart.items.length) {
-            return true;
-          }
+  canActivate(): Observable<boolean> | boolean {
+    return this.store.select(fromReducers.selectCart).pipe(
+      map((cart) => {
+        if (cart.items && cart.items.length) {
+          return true;
+        }
 
-          this.router.navigate(['/']);
-          return false;
-        })
-      );
+        this.router.navigate(['/']);
+        return false;
+      })
+    );
   }
-  
 }
